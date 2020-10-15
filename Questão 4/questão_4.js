b52f5d3e985bd7fee295fd3508ab94fe77ec42a1
Mateus300 <mateus.souza.pro@gmail.com>
2020-10-14 23:37:04 -0300

    Questão 4 completa

---
+++ Questão 4/questão_4.js
@@ -0,0 +1,7 @@
+function divisaoResto ( num1, num2 ) {
+    resultado = num1 / num2
+    resto = num1 % num2
+    return "O resultado é " + resultado + " e o resto da divisão é " + resto
+}
+
+console.log( divisaoResto (8, 3))
